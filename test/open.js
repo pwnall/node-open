3eb93d9e9d85ea85bf6f4ae6a45f7b1589fad031
J. Jordan <jordan@styosis.com>
2012-04-15 07:38:39 -0400

    [test] add https uri test

--- test/open.js
+++ test/open.js
@@ -6,20 +6,21 @@ describe('open', function() {
   function pathTo(asset) {
     return path.join(__dirname, 'support', asset);
   }
-  var html = pathTo('asset.html');
-  var jpeg = pathTo('asset.jpg');
-  var txt = pathTo('asset.txt');
 
   it('should open html file in default browser', function(done) {
-    open(html, done);
+    open(pathTo('asset.html'), done);
+  });
+
+  it('should open https uris in default browser', function(done) {
+    open('https://github.com/fixedset/open.js', done);
   });
 
   it('should open image file in default image viewer', function(done) {
-    open(jpeg, done);
+    open(pathTo('asset.jpg'), done);
   });
 
   it('should open txt file in default text editor', function(done) {
-    open(txt, done);
+    open(pathTo('asset.txt'), done);
   });
 
 });
