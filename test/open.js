a823af8abeec0804453846bfaff9f630ce0a6c18
Jay Jordan <jjrdn@styosis.com>
2012-06-19 16:43:55 -0400

    Change to unix file format on text files

--- test/open.js
+++ test/open.js
@@ -1,32 +1,47 @@
+/*global describe it */
+
 var path = require('path');
 var open = require('../');
 
-describe('open', function() {
+// NOTE: this is not really an automated test suite.
+// It does not check that the applications are actually opened. This needs
+// to be verified manually.  It *does* ensure that the opener process
+// does not return an error, and this is a good proxy for success for
+// much less effort.
+//
+// the xdg-open script behaves differently than start and open in that
+// it does not return after opening the process, it waits until the child
+// process exits.  Because of this, the callback parameter is not documented
+// in the readme at this time.
+describe('open', function () {
   function pathTo(asset) {
     return path.join(__dirname, 'support', asset);
   }
 
-  it('should open html file in default browser', function(done) {
+  it('should open html file in default browser', function (done) {
     open(pathTo('asset.html'), done);
   });
 
-  it('should open https uris in default browser', function(done) {
-    open('https://github.com/fixedset/open.js', done);
+  it('should open https uris in default browser', function (done) {
+    open('https://github.com/jjrdn/node-open.js', done);
   });
 
-  it('should open image file in default image viewer', function(done) {
-    open(pathTo('asset.jpg'), done);
+  //it('should open image file in default image viewer', function (done) {
+    //open(pathTo('asset.jpg'), done);
+  //});
+
+  it('should open txt file in default text editor', function (done) {
+    open(pathTo('asset.txt'), function (error) {
+      console.log('yep editor is open');
+      done();
+    });
   });
 
-  it('should open txt file in default text editor', function(done) {
-    open(pathTo('asset.txt'), done);
-  });
-  
-  it('should open files with spaces', function(done) {
+  it('should open files with spaces', function (done) {
     open(pathTo('with space.html'), done);
   });
-  
-  it('should open files with quotes', function(done) {
+
+  it('should open files with quotes', function (done) {
     open(pathTo('with"quote.html'), done);
   });
 
